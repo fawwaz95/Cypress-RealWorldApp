240a176a4792f4f2a0de98a7af045fb79e3e0b5f
fawwaz95 <fawwaz_95@hotmail.com>
2025-12-22 22:18:55 -0500

    added listen for web:3001

--- wait-and-run.sh
+++ wait-and-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "🔁 Waiting for web to be ready..."
-npx wait-on http://web:3000
-echo "✅ Web is up! Running Cypress..."
+echo "🔁 Waiting for web and API to be ready..."
+npx wait-on http://web:3000 http://web:3001
+
+echo "✅ Services are up! Running Cypress..."
 npx cypress run --reporter junit --reporter-options "mochaFile=results/test-results.xml,toConsole=true"
